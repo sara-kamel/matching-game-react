5734d3051812c5305ef21093cc1c98f08c6fc935
Sara Kamel <sarakamel591@gmail.com>
2024-06-03 16:28:52 -0700

    Improve the functions of changing the levels and set image as a background

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
-import { useState, useEffect } from "react";
-import "./App.css";
-import MatchingList from "./MatchingList";
-import { styled } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Unstable_Grid2";
-import { shuffleArray, setLevel, sliceArray } from "./helper";
-import { Button, ButtonGroup, Stack } from "@mui/material";
+import { useState, useEffect } from 'react';
+import './App.css';
+import MatchingList from './MatchingList';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Unstable_Grid2';
+import { shuffleArray, setLevel } from './helper';
+import { Button, ButtonGroup, Stack } from '@mui/material';
 
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
   padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-  height: "150px",
-  width: "150px",
+  height: '150px',
 }));
- const list = [...MatchingList]
-const levelOneItems = sliceArray(list, 0, 4);
+
 function App() {
-  const [subheader, setSubheader] = useState("Level One");
+  const [subheader, setSubheader] = useState('Level One');
+  const [itemsCount, setItemsCount] = useState(4);
   const [shuffledItems, setShuffledItems] = useState(() => {
-    return shuffleArray(levelOneItems);
+    return shuffleArray([...setLevel(MatchingList, itemsCount)]);
   });
   const [matchedItems, setMatchedItems] = useState([]);
 
@@ -31,7 +28,7 @@ function App() {
         const newList = [...shuffledItems];
         for (let i = 0; i < matchedItems.length; i++) {
           newList.forEach((item) => {
-            if (item.name === matchedItems[i].name) item.status = "matching";
+            if (item.name === matchedItems[i].name) item.status = 'matching';
           });
         }
         setShuffledItems(newList);
@@ -40,35 +37,30 @@ function App() {
   }, [matchedItems]);
 
   useEffect(() => {
-    const level = setLevel(MatchingList, subheader);
+    const level = setLevel(MatchingList, itemsCount);
     setShuffledItems(level);
   }, [subheader]);
 
   function comparisonItems(item) {
-    if (item.status === "matching") return;
+    if (item.status === 'matching') return;
     const newList = [...shuffledItems];
-    const filteredItems = newList.filter((i) => i.status === "show");
+    const filteredItems = newList.filter((i) => i.status === 'show');
     let shownItem = null;
     if (filteredItems.length > 1) {
-      filteredItems.forEach((i) => (i.status = "init"));
+      filteredItems.forEach((i) => (i.status = 'init'));
     } else if (filteredItems.length === 1) {
       shownItem = filteredItems[0];
     }
-    item.status = "show";
+    item.status = 'show';
     if (shownItem && shownItem.name === item.name && shownItem.id !== item.id) {
       setMatchedItems([item, shownItem]);
     }
     setShuffledItems(newList);
   }
 
-  const handleChangeLevel = () => {
-    if (subheader === "Level One") {
-      setSubheader("Level Two");
-    } else if (subheader === "Level Two") {
-      setSubheader("Level Three");
-    } else {
-      setSubheader("Level One");
-    }
+  const handleChangeLevel = (level, num) => {
+    setSubheader(level);
+    setItemsCount(num);
   };
   return (
     <>
@@ -77,55 +69,57 @@ function App() {
       </Box>
       <Stack direction="row" justifyContent="center">
         <ButtonGroup variant="contained" aria-label="Basic button group">
-          <Button onClick={() => setSubheader("Level One")}>Easy</Button>
-          <Button onClick={() => setSubheader("Level Two")}>Medium</Button>
-          <Button onClick={() => setSubheader("Level Three")}>Hard</Button>
+          <Button onClick={() => handleChangeLevel('Level One', 4)}>Easy</Button>
+          <Button onClick={() => handleChangeLevel('Level Two', 6)}>Medium</Button>
+          <Button onClick={() => handleChangeLevel('Level Three', 9)}>Hard</Button>
         </ButtonGroup>
       </Stack>
       <Box sx={{ flexGrow: 1 }}>
-      <h4 className="subheader">{subheader}</h4>
+        <h4 className="subheader">{subheader}</h4>
         <Grid
           container
-          width = {{ xs: "80%", md: subheader === "Level One" ? "50%" : "70%" }}
+          width={{ xs: '100%', sm: '90%', md: subheader === "Level One"? '50%': '75%' }}
           margin="auto"
           spacing={{ xs: 2, md: 2 }}
-          columns={{ xs: 4, sm: 8, md: 12 }}
-        >
-     
+          columns={{ xs: 4, sm: 8, md: 12 }}>
           {shuffledItems.map((item) => (
-            <Grid
-              key={item.id}
-              xs={2}
-              sm={2}
-              md={subheader === "Level One" ? 3 :2}
-            >
+            <Grid key={item.id} xs={2} sm={2} md={subheader === "Level One"? 3 : 2}>
               <Item
-                className={
-                  item.status === "matching" ? "matched-item" : "shown-item"
-                }
+                className={item.status === 'matching' ? 'matched-item' : 'shown-item'}
                 onClick={() => {
                   comparisonItems(item);
-                }}
-              >
-                {item.status === "show" ? (
-                  <img
-                   height= "150px"
-                    width= "150px"
-                    src={item.image}
-                    alt={item.name}
-                  />
+                }}>
+                {item.status === 'show' ? (
+                  <div
+                    style={{
+                      backgroundImage: `url(${item.image})`,
+                      backgroundSize:"cover",
+                      backgroundPosition: 'center',
+                      height: "100%",
+                    }}>
+                  </div>
                 ) : (
-                  ""
+                  ''
                 )}
               </Item>
             </Grid>
           ))}
         </Grid>
-        <br/>
+        <br />
         <Stack direction="row" justifyContent="center">
-        <Button variant="contained" onClick={() => handleChangeLevel()}>
-          Next Level
-        </Button>
+          <Button
+            variant="contained"
+            onClick={() => {
+              if (subheader === 'Level One') {
+                handleChangeLevel('Level Two', 6);
+              } else if (subheader === 'Level Two') {
+                handleChangeLevel('Level Three', 9);
+              } else {
+                handleChangeLevel('Level One', 4);
+              }
+            }}>
+            Next Level
+          </Button>
         </Stack>
       </Box>
     </>
--- src/helper.js
+++ src/helper.js
@@ -1,37 +1,25 @@
 import { v4 as uuidv4 } from 'uuid';
 
-export function sliceArray(list , num1, num2){
+export function sliceArray(list, num1, num2) {
   const sliceList = list.slice(num1, num2);
-  const  duplicateList = [...sliceList, ...sliceList]
-  const newListWithId = duplicateList.map(item => {
-    return { ...item, id: uuidv4()};
+  const duplicateList = [...sliceList, ...sliceList];
+  const newListWithId = duplicateList.map((item) => {
+    return { ...item, id: uuidv4() };
   });
-return newListWithId ;
+  return newListWithId;
 }
 
 export const shuffleArray = (array) => {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
-    array[i].status = "init";
+    array[i].status = 'init';
   }
   return array;
 };
 
-export function setLevel(list, subheader ) {
-const NewCopyArray = [...list];
-const levelOneItems = sliceArray(NewCopyArray, 0, 4);
-const levelTwoItems = sliceArray(NewCopyArray, 0, 6);
-const levelThreeItems = sliceArray(NewCopyArray, 0, 9)
-  let level = null;
-  if (subheader === "Level One") {
-    level = () => shuffleArray(levelOneItems);
-  }
-  if (subheader === "Level Two") {
-    level = () => shuffleArray(levelTwoItems);
-  }
-  if (subheader === "Level Three") {
-    level = () => shuffleArray(levelThreeItems);
-  }
-  return level;
+export function setLevel(list, num) {
+  const NewCopyArray = [...list];
+  const levelItems = sliceArray(NewCopyArray, 0, num);
+  return shuffleArray([...levelItems]);
 }
